fb64b20c61abfa80109e6d22998d6c0f1ead37de
13817094576 <393873549@qq.com>
2016-06-29 10:43:17 +0800

    Code for recording info on root caller Activity class added

--- src/edu/fudan/JimpleKeyword/RootCallerInspector.java
+++ src/edu/fudan/JimpleKeyword/RootCallerInspector.java
@@ -1,12 +1,16 @@
 package edu.fudan.JimpleKeyword;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
 
+import soot.SootClass;
 import soot.SootMethod;
 import soot.Unit;
+import soot.jimple.InvokeExpr;
+import soot.jimple.InvokeStmt;
 
 /**
 
@@ -28,6 +32,122 @@ class RootCallerInspector
 		for (String curRootCallerClass : rootCallerClass)
 		{
 			System.out.println(curRootCallerClass);
+		}
+	}
+
+	/**
+
+		Check if the given class is a sub-class of framework class which
+		manages content display, such as Activity, Fragment.
+		
+	*/
+	private boolean isChildOfDisplayableClass(SootClass sootClass)
+	{
+		// Check classes from the lowest class hierarchy
+		SootClass curClass = sootClass;
+		while (true)
+		{
+			//
+			// Check if the name of the class contains
+			// name of class managing content display
+			String curClassName = curClass.getName();
+			
+			if (curClassName.contains("android.app.Activity")
+				|| curClassName.contains("android.app.Fragment"))
+			{
+				return true;
+			}
+			
+			//
+			// Otherwise, check the superclass of current class
+			if (curClass.hasSuperclass())
+			{
+				curClass = curClass.getSuperclass();
+			}
+			else
+			{
+				// We have arrived at the top of class hierarchy
+				// so the given class isn't a child of Activity
+				return false;
+			}
+		}
+	}
+	
+	private SootMethod getOnCreateMethod(SootClass sootClass)
+	{
+		Iterator<SootMethod> methodIter = sootClass.methodIterator();
+		while (methodIter.hasNext())
+		{
+			SootMethod m = methodIter.next();
+			
+			if (m.getName().contains("onCreate"))
+			{
+				return m;
+			}
+		}
+		
+		//
+		// No method named onCreate
+		return null;
+	}
+	
+	private void inspectRootCallerClass(SootClass sootClass)
+	{
+		//
+		// We only care about root caller class which is a 
+		// child of class managing content display
+		if (!isChildOfDisplayableClass(sootClass))
+		{
+			return;
+		}
+		
+		//
+		// Figure out the onCreate method in an Activity class
+		SootMethod m = getOnCreateMethod(sootClass);
+		if (m == null)
+		{
+			System.err.println("[WARN] No onCreate() method in Activity class: " + sootClass.getName());
+			return;
+		}
+		
+		// Construct active body for some method
+		if (!m.hasActiveBody() && m.isConcrete())
+		{
+			m.retrieveActiveBody();
+		}
+			
+		// Skip method without active body
+		if (!m.hasActiveBody())
+		{
+			return;
+		}
+		
+		//
+		// Traverse the statements in onCreate method
+		// to extract setContentView statement
+		Iterator<Unit> unitIter = m.getActiveBody().getUnits().iterator();
+		while (unitIter.hasNext())
+		{
+			//
+			// We only care about invoke statement
+			Unit unit = unitIter.next();
+			if (!(unit instanceof InvokeStmt))
+			{
+				continue;
+			}
+			
+			//
+			// We only care about invoke of setContentView method
+			InvokeExpr invokeExpr = ((InvokeStmt)unit).getInvokeExpr();
+			if (!invokeExpr.getMethod().getName().contains("setContentView"))
+			{
+				continue;
+			}
+			
+			//
+			// Record root caller and its resource ID
+			String resourceId = invokeExpr.getArg(0).toString();
+			rootCallerClass.add(sootClass.getName() + ',' + resourceId);
 		}
 	}
 	
@@ -52,9 +172,8 @@ class RootCallerInspector
 			//
 			// Current method is a root caller
 			
-			// Record name of root caller class
-			String rootCallerClassName = m.getDeclaringClass().getName();
-			rootCallerClass.add(rootCallerClassName);
+			// Inspect and record related information of the class
+			inspectRootCallerClass(m.getDeclaringClass());
 		}
 		else
 		{
